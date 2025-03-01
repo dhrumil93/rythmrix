385c3932b5545277152f892b8e183784a1cd2e8e
dhrumil93 <dhrumilpatel0093@gmail.com>
2025-03-01 19:00:44 +0530

    updating layout

--- app/(app)/tests/_layout.jsx
+++ app/(app)/tests/_layout.jsx
@@ -8,15 +8,20 @@ export default function TestsLayout() {
       }}
     >
       <Stack.Screen name="index" />
-      <Stack.Screen name="12l-ecg" />
+      <Stack.Screen name="12l-ecg/index" />
       <Stack.Screen name="12l-ecg/placement" />
-      <Stack.Screen name="12l-ecg/recording" />
+      <Stack.Screen name="12l-ecg/user-details" />
+      <Stack.Screen name="12l-ecg/v1/index" />
+      <Stack.Screen name="12l-ecg/v2" />
+      <Stack.Screen name="12l-ecg/v3" />
+      <Stack.Screen name="12l-ecg/ii" />
+      <Stack.Screen name="12l-ecg/l" />
+      <Stack.Screen name="12l-ecg/member-report" />
       <Stack.Screen name="heart-risk" />
       <Stack.Screen name="hrv" />
       <Stack.Screen name="live-monitor" />
       <Stack.Screen name="lead2-ecg" />
       <Stack.Screen name="hyperkalemia" />
-      <Stack.Screen name="reports" />
     </Stack>
   );
-} 
+}
